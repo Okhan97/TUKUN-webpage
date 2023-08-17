fe0c4d94db50b39acc5458d9b09182bcf4f197f3
Okhan97 <26289334+Okhan97@users.noreply.github.com>
2023-08-17 02:44:46 -0400

    roadmap advance

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,49 @@
 import "../styles/about.css";
 
+const Item = () => {
+  return (
+    <div className=" item column gap-5">
+      <div className="item-dot" />
+      <div className="item-square" />
+    </div>
+  );
+};
+
 export default function About() {
-  return <div className="about-container"></div>;
+  return (
+    <div className="about-container row">
+      <div className="column left-side-container gap-5">
+        <div className="row gap-3 title-container">
+          <div className="title-dot" />
+          <h1 className="title">NOSOTROS SOMOS</h1>
+        </div>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet, consectetuer adi- piscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat.
+        </p>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet, consectetuer adi- piscing elit, sed diam
+          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+          volutpat.
+        </p>
+        <div className="column gap-6 items-container">
+          <div className="row gap-10">
+            <Item />
+            <Item />
+          </div>
+          <div className="row gap-10">
+            <Item />
+            <Item />
+          </div>
+        </div>
+        <div className="manifest-container button">
+          <span className="manifest-title">MANIFIESTO</span>
+          <div className="manifest-big-yellow-line" />
+          <div className="manifest-small-yellow-line" />
+        </div>
+      </div>
+      <div className="image"></div>
+    </div>
+  );
 }
--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -1,5 +1,28 @@
 import "../styles/roadmap.css";
 
+const text =
+  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet";
+
+const Folder = ({ number, text }) => {
+  return (
+    <div className="folder-container column">
+      <div className="folder-title">PASO {number}</div>
+      <div className="folder-body">{text}</div>
+    </div>
+  );
+};
 export default function Roadmap() {
-  return <h1>Roadmap</h1>;
+  return (
+    <div className="roadmap-container column">
+      <div className="title">
+        <h1>ROADMAP</h1>
+      </div>
+      <div className="folders-row row gap-10">
+        <Folder number={1} text={text} />
+        <Folder number={2} text={text} />
+        <Folder number={3} text={text} />
+        <Folder number={4} text={text} />
+      </div>
+    </div>
+  );
 }
